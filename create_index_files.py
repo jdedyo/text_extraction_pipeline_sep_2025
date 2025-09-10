0c910daabbf7fa5739e74bb539766fa814ffbcd3
Johnny Dedyo <johnny.dedyo@yale.edu>
2025-09-09 21:03:41 -0400

    update create_index_files after convo w cormac

--- create_index_files.py
+++ create_index_files.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import csv
 from pathlib import Path
+from tqdm import tqdm
 
 # %%
 def load_dol_file_to_df(year: int, index_files_path: Path):
@@ -26,6 +27,7 @@ def load_dol_file_to_df(year: int, index_files_path: Path):
 
     df.rename(columns={'id': 'ack_id'}, inplace=True)
     df["ack_id"] = df["ack_id"].astype(str)
+    df["filing_year"] = df["filing_year"].astype(int)
     df = df.apply(lambda col: col.map(lambda x: x.strip() if isinstance(x, str) else x))
     df["sponsor_name"] = df["sponsor_name"].str.strip('"')
     return df
@@ -50,15 +52,19 @@ def get_acceptable_ack_ids_from_index_file(filepath: Path):
 YEARS = list(range(1999, 2025))
 INDEX_FILES_PATH = Path("./index_files")
 
-SAVEPATH = Path('./universe')
+SAVEPATH = INDEX_FILES_PATH / 'universe'
 SAVEPATH.mkdir(parents=True, exist_ok=True)
 
 if __name__ == '__main__':
     
     good_ack_ids = get_acceptable_ack_ids_from_index_file(INDEX_FILES_PATH / 'merged_sh_h.dta')
 
-    for year in YEARS:
-        print(f"Creating universe_{year}.csv...\n")
+    for year in tqdm(
+        YEARS,
+        total=len(YEARS),
+        desc=f"Creating universe_all.csv"
+    ):
+        print(f"Reading in {year}.txt...\n")
 
         dol_df = load_dol_file_to_df(year, INDEX_FILES_PATH / 'dol_index_files')
         filtered_df = dol_df.loc[dol_df.ack_id.isin(good_ack_ids)]
@@ -69,10 +75,27 @@ if __name__ == '__main__':
         except NameError:
             universe = filtered_df
         print(f"Concatenated {year} to universe_all!\n")
-
-        filtered_df.to_csv(SAVEPATH / f'universe_{year}.csv', index=False)
-        
-        print(f"Saved universe_{year}.csv!\n")
-        
+    
+    print(f"Saving universe_all.csv")
+    universe = universe.drop_duplicates()
+    universe["filing_year"] = pd.to_numeric(universe["filing_year"], errors="coerce")
     universe.to_csv(SAVEPATH / 'universe_all.csv', index=False)
     print("Saved universe_all.csv!\n")
+
+    all_valid = universe["filing_year"].notna() & universe["filing_year"].astype(str).str.strip().ne("")
+    all_valid = all_valid.all()
+
+    print(f"Are all filing_year entries filled in? {all_valid}")
+
+    for year in tqdm(
+        YEARS,
+        total=len(YEARS),
+        desc=f"Setting up universe_YYYY.csv files"
+    ):
+
+        mask = universe["filing_year"].eq(year)  # boolean Series
+        df_year = universe.loc[mask]
+
+        df_year.to_csv(SAVEPATH / f'universe_{year}.csv', index=False)
+        
+        print(f"Saved universe_{year}.csv!\n")
